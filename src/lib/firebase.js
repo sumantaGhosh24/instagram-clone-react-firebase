42fdf70978e1aea09bffefc1bb42cf5ac1fcd9c5
sumanta ghosh <sumantaghosh5333@gmail.com>
2022-10-06 20:34:33 +0530

    complete

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import Firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+const firebase = Firebase.initializeApp(config);
+const {FieldValue} = Firebase.firestore;
+
+export {firebase, FieldValue};
